69361ae73a964962173bdd7e92a79d01c5f23cda
Olgasyla <olgasyla@gmail.com>
2024-08-12 15:53:59 +0200

    add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import './App.css'
 import CharacterGallery from "./components/CharacterGallery.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {characters} from "./Characters.ts";
+import axios from "axios";
+import {Character} from "./types/RickAndMortyCharacter.ts";
 
 export default function App() {
+
+    const [characters, setCharacters] = useState<Character[]>([]);
     const [searchText, setSearchText] = useState("");
 
     const filteredCharacters = characters
         .filter((character) => character.name.toLowerCase().includes(searchText.toLowerCase()));
+
+    useEffect(() => {
+        loadCharacters()}, [])
+
+
+    const loadCharacters = () => {
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then((response) => {
+                    setCharacters(response.data.results)
+                }
+            );
+    }
 
     return (
         <>
